f22b33d7f55e3b0205326367ede6fadc510f8c98
DYuHLin <damianver2000@gmail.com>
2025-05-01 16:12:50 +0200

    Currently working on users being able to post.

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
-import Sidebar from './components/home/Sidebar';
+import React, { useState } from 'react';
+import PostImg from './components/Misc/PostImg'
 
 const Create = () => {
+    const [text, setText] = useState('')
+    const [video, setVideo] = useState('')
+    const [link, setLink] = useState('')
+    const [youtube, setYoutube] = useState('')
+    const [img, setImg] = useState([])
+
+    const [youtubeShow, setYoutubeShow] = useState(true)
+    const [linkShow, setLinkShow] = useState(false)
+    const [videoShow, setVideoShow] = useState(false)
+    const [imgShow, setImgShow] = useState(false)
+
+    const createPost = (e) => {
+        e.preventDefault()
+        // const post = {text, video, link, poster}
+    }
+
     return (
         <section className='home-page'>
             <div className='feed'>
@@ -10,18 +26,20 @@ const Create = () => {
                 </div>
 
                 <div className='feed-post'>
-                    <form>
-                        <textarea placeholder='Write something' rows='8'></textarea>
-                        {/* <input type='text' placeholder='link' className='post-input'/>
-                        <input type='text' placeholder='video link' className='post-input'/> */}
+                    <form onSubmit={createPost}>
+                        <textarea placeholder='Write something' rows='8' onChange={(e) => setText(e.target.value)}></textarea>
+                        <input type='text' placeholder='link' className={`post-input ${linkShow ? '' : 'hidden'}`} onChange={(e) => setLink(e.target.value)}/>
+                        <input type='text' placeholder='video link' className={`post-input ${videoShow ? '' : 'hidden'}`} onChange={(e) => setVideo(e.target.value)}/> 
+                        <input type='text' placeholder='youtube link' className={`post-input ${youtubeShow ? '' : 'hidden'}`} onChange={(e) => setYoutube(e.target.value)}/> 
+                        <PostImg setImage={setImg} imgBox={imgShow}/>
                         <button className='post-btn'>Post</button>
                     </form>
                 </div>
                 <div className='filter-feed'>
-                    <i className='bx bx-link post-op'/>
-                    <i class='bx bx-pencil post-op'/>
-                    <i class='bx bx-image post-op' />
-                    <i class='bx bxs-videos post-op' />
+                    <i className='bx bx-link post-op' onClick={() => {setLinkShow(!linkShow); setImgShow(false); setYoutubeShow(false); setVideoShow(false);}}/>
+                    <i className='bx bxl-youtube' onClick={() => {setLinkShow(false); setImgShow(false); setYoutubeShow(!youtubeShow); setVideoShow(false);}}/>
+                    <i className='bx bx-image post-op' onClick={() => {setLinkShow(false); setImgShow(!imgShow); setYoutubeShow(false); setVideoShow(false);}}/>
+                    <i className='bx bxs-videos post-op' onClick={() => {setLinkShow(!linkShow); setImgShow(false); setYoutubeShow(false); setVideoShow(false);}}/>
                 </div>
             </div>
         </section>
--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -12,16 +12,16 @@ exports.getPost = asyncHandler(async (req, res, next) => {
 })
 
 exports.createPost = asyncHandler(async (req, res, next) => {
-    const {text, video, link, date, poster} = req.body
+    const {text, video, link, date, poster, youtube} = req.body
 
-    await postModel.create(text, video, link, date, poster)
+    await postModel.create(text, video, link, date, poster, youtube)
     return res.json('created')
 })
 
 exports.updatePost = asyncHandler(async (req, res, next) => {
-    const {text, video, link, date, poster} = req.body
+    const {text, video, link, date, poster, youtube} = req.body
 
-    await postModel.update(text, video, link, date, poster, req.params.id)
+    await postModel.update(text, video, link, date, poster, youtube, req.params.id)
     return res.json('updated')
 })
 
